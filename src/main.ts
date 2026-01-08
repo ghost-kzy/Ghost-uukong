aa53d4c56ccb42880468463acd68a2cb2be5ccbd
uu-kong <1056568558@qq.com>
2026-01-08 21:39:47 +0800

    feat: initial commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
+
+const x = () => {
+  console.log('这是没有格式化的代码')
+}
+x()
